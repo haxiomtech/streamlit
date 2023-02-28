8812a11c2498ef6932a846cb5835912503df3b3c
Haxiom Tech <119424591+haxiomtech@users.noreply.github.com>
2023-02-28 11:44:35 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,35 @@
+import numpy as np
+import altair as alt
+import pandas as pd
 import streamlit as st
 
-st.header('Projeto de 30 dias')
+st.header('st.write')
 
-if st.button('Diga Olá'):
-     st.write('Teste')
-else:
-     st.write('Adeus')
+# Example 1
+
+st.write('Hello, *World!* :sunglasses:')
+
+# Example 2
+
+st.write(1234)
+
+# Example 3
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# Example 4
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# Example 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
